e0569e6bd702a6dae5e4f3f3080eb49ab12da0ce
Twopercent <twopercent051@protonmail.com>
2023-01-02 19:52:48 +0300

    v2

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
 from flask import Flask, jsonify, request
 from requester import get_currency
-from apispec.ext.marshmallow import MarshmallowPlugin
-from apispec import APISpec
-from flask_apispec.extension import FlaskApiSpec
+
 
 app = Flask(__name__)
 
 client = app.test_client()
 
-docs = FlaskApiSpec()
-docs.init_app(app)
-
-app.config.update({
-    'APISPEC_SPEC': APISpec(
-        title='currency',
-        version='v1',
-        openapi_version='2.0',
-        plugins=[MarshmallowPlugin()]
-    ),
-    'APISPEC_SWAGGER_URL': '/swagger/'
-})
 
 @app.route('/api/rates', methods=['GET'])
 def get_list():
